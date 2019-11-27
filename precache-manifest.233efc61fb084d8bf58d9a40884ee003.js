6ba5c3dc17fa5320fff7fcea1fd970ec0202fd17
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-27 10:35:11 +0000

    Updates

---
+++ precache-manifest.233efc61fb084d8bf58d9a40884ee003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159dceab07a0e1515a3f0913a41237cf",
+    "url": "/instaclone-group-project/index.html"
+  },
+  {
+    "revision": "fc6d73446f7bfc38c4c5",
+    "url": "/instaclone-group-project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4d9944660d251450c252",
+    "url": "/instaclone-group-project/static/js/2.7781532a.chunk.js"
+  },
+  {
+    "revision": "fc6d73446f7bfc38c4c5",
+    "url": "/instaclone-group-project/static/js/main.adcabcef.chunk.js"
+  },
+  {
+    "revision": "46e7ac0648e23567b04d",
+    "url": "/instaclone-group-project/static/js/runtime-main.cddb4a7b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/instaclone-group-project/static/media/logo.25bf045c.svg"
+  }
+]);
