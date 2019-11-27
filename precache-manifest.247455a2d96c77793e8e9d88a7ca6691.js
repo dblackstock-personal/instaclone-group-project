133879969e9c5e17ae97d04e0e6f93f9c8609a66
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-27 10:37:41 +0000

    Updates

--- precache-manifest.247455a2d96c77793e8e9d88a7ca6691.js
+++ precache-manifest.247455a2d96c77793e8e9d88a7ca6691.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "159dceab07a0e1515a3f0913a41237cf",
+    "revision": "5c2524712f80d32e29e6ceca02ae45a7",
     "url": "/instaclone-group-project/index.html"
   },
   {
-    "revision": "fc6d73446f7bfc38c4c5",
+    "revision": "9641f3089c9aab18c926",
     "url": "/instaclone-group-project/static/css/main.b100e6da.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/instaclone-group-project/static/js/2.7781532a.chunk.js"
   },
   {
-    "revision": "fc6d73446f7bfc38c4c5",
-    "url": "/instaclone-group-project/static/js/main.adcabcef.chunk.js"
+    "revision": "9641f3089c9aab18c926",
+    "url": "/instaclone-group-project/static/js/main.47187637.chunk.js"
   },
   {
     "revision": "46e7ac0648e23567b04d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/instaclone-group-project/precache-manifest.233efc61fb084d8bf58d9a40884ee003.js"
+  "/instaclone-group-project/precache-manifest.247455a2d96c77793e8e9d88a7ca6691.js"
 );
 
 self.addEventListener('message', (event) => {
