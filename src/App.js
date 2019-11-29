80d09cdb938584570c4b3ae8848952e61b4d5e54
Catherine Harrison <mrs.wizrd@gmail.com>
2019-11-29 12:35:16 +0000

    amending the App.js and App.css to merge my additions to the master.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 import Feed from './components/Feed';
+import TopNav from './components/TopNav';
+import BottomNav from './components/BottomNav';
+import Stories from './components/Stories';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends Component {  //these are for the desktop website
+  handleClick = (e) => {
+    if (e.type == 'mousedown') {
+      e.target.style.filter = 'invert(50%)';
+    } else {
+      e.target.style.filter = 'none';
+    }
+  }  render() {
     return (
       <div>
-        <div>
-         <Feed />
-        </div>
+        <TopNav logoClick={this.handleClick}/>
+        <div id="main-page">
+            <Feed />
+            <Stories/>
+        </div> 
+            <BottomNav />
       </div>
     )
   }
